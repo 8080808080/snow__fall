01e24300c393600ef98685c187aefda5570f40d0
8080808080 <82901527+8080808080@users.noreply.github.com>
2022-05-20 22:30:45 +0530

    Add files via upload

---
+++ SnowFalling-C33-Template-main/sketch.js
@@ -0,0 +1,26 @@
+
+var bg;
+
+function prelord(){
+bg = loadImage("images/snow3.jpg");
+}
+
+function setup() {
+  createCanvas(1000,500);
+  createSprite(400, 200, 50, 50);
+
+ 
+
+}
+
+function draw() {
+  background("bg");  
+
+
+
+  drawSprites();
+
+
+
+
+}
